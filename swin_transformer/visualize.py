9e94114a9d0024b640628e18020a6eb764be3976
Mohammed Omer <mohammed.omer.77omr@gmail.com>
2024-10-25 23:23:27 +0530

    Update visualize.py

--- swin_transformer/visualize.py
+++ swin_transformer/visualize.py
@@ -58,4 +58,40 @@ def display_random_images(dataset: torch.utils.data.dataset.Dataset,
         title = list(targ_label)
         if display_shape:
             title = title + f"\nshape: {targ_image_adjust.shape}"
-        plt.title(title)     
+        plt.title(title)    
+
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
